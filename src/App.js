b0a4e9d3b5ded0adea728bcb90de50479afcce32
BalesabuGodugu <godugubalesabu@gmail.com>
2018-07-18 23:37:12 +0530

    updated packages

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import AppBar from '@material-ui/core/AppBar';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginLeft: -18,
+    marginRight: 10,
+  },
+};
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <AppBar position="static"
+              title="React public website"/>
       </div>
     );
   }
